d86302c01450670b3895f6c7595b21482da8234a
Akio Suzuki <116661015+Kio-Suzuki@users.noreply.github.com>
2023-03-15 14:47:11 -0300

    Laco1 created

---
+++ Aula 01/src/application/Laco1.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Laco1 {
+
+	public static void main(String args[]) {
+		
+		int a = 0, b = 10;
+		
+		for(a = 0; a <= b; b--) {
+			System.out.println("O Valor de B dentro do FOR é: "+b);		
+		}
+	}
+	
+}
---
+++ Aula 01/src/application/Laco2.java
@@ -0,0 +1,16 @@
+package application;
+
+public class Laco2 {
+
+	public static void main(String args[]) {
+		
+		int a = 0, b = 15;
+		
+		while(b >= a) {
+			b--;
+			System.out.println("O valor de B dentro do while é: " +b);
+		}
+
+	}
+
+}
