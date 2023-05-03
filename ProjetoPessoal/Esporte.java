df1b8f5b572c815360d1867aee8a8fbaade013ea
Akio Suzuki <116661015+Kio-Suzuki@users.noreply.github.com>
2023-05-03 17:06:39 -0300

    Update Esporte.java

--- ProjetoPessoal/Esporte.java
+++ ProjetoPessoal/Esporte.java
@@ -2,28 +2,38 @@
 
 public class Esporte extends ConteudoStreaming{
 
-	private String esporte;
 	private String liga;
+	private String custoAdic;
+	private Double custo
 	
 	public Esporte(){
-		esporte = "";
 		liga = "";
+		custoAdic = "";
+		custo 0.0;
 	}
 
-	public String getEsporte(){
-		return esporte;
-	}
-	
 	public String getLiga(){
 		return liga;
 	}
 
-	public void setEsporte(String esporte){
-		this.esporte = esporte;
+	public String getCustoAdic(){
+		return custoAdic;
 	}
 
+	public Double getCusto(){
+		return custo
+	}
+		
 	public void setLiga(String liga){
 		this.liga = liga;
+	}
+
+	public void setCustoAdic(String custoAdic){
+		this.custoAdic = custoAdic;
+	}
+
+	public void setCusto(Double custo)
+		this.custo = custo;
 	}
 	
 }
